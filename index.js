5cb3cbd20fcb972aaf7e43fe41d88f2032fc383d
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 21:32:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  return document.getElementById("grand-node").querySelector("div div div div")
+  return document.getElementById("grand-node").querySelector("div div div div div")
 }
 
