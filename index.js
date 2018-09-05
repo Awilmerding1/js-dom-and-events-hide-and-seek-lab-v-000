e7cec7aa2bfe511467e06af5fe4a30c123e1ddae
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-05 21:33:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+	const r = document.querySelectorAll('.ranked-list')
+    for (let i = 0; i < parseInt(r.length); i++) {
+      r[i].innerHTML = (i + 1).toString()
+    }
+}
+
+function deepestChild() {
+  return document.getElementById("grand-node").querySelector("div div div div div")
+}
+
